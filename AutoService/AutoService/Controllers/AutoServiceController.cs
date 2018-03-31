a1c9a29b8845170b23b7377c31807a6f0409c571
Oleh <oleh.markevych@gmail.com>
2018-03-31 21:40:55 +0300

    Add main controllers

---
+++ AutoService/AutoService/Controllers/AutoServiceController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoService.Controllers
+{
+    public class AutoServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult UpdateContactInfo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void UpdateContactInfo(AutoService.Models.AutoService client)
+        {
+
+        }
+
+    }
+}
--- AutoService/AutoService/Controllers/ClientController.cs
+++ AutoService/AutoService/Controllers/ClientController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AutoService.Models;
 
 namespace AutoService.Controllers
 {
@@ -11,6 +12,61 @@ namespace AutoService.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult AddAdvertisement()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void AddAdvertisement(int clientId, Advertisement advertisement)
+        {
+
+        }
+
+        public ActionResult RegisterClient()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void RegisterClient(Client client)
+        {
+
+        }
+
+        public ActionResult UpdateAdvertisement()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void UpdateAdvertisement(Client client)
+        {
+
+        }
+
+        public ActionResult RemoveAdvertisement()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void RemoveAdvertisement(Client client)
+        {
+
+        }
+
+        public ActionResult UpdateContactInfo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void UpdateContactInfo(Client client)
+        {
+
         }
     }
 }
