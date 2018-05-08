e5825ff6330ef50f82621cde2b43520629c1cf9f
GreatDragonTitan <titanivan25@gmail.com>
2018-05-08 22:42:16 +0300

    Some edit

--- Auto_Service/Auto_Service/Models/IdentityModels.cs
+++ Auto_Service/Auto_Service/Models/IdentityModels.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Auto_Service.Models
 {
-    // В профиль пользователя можно добавить дополнительные данные, если указать больше свойств для класса ApplicationUser. Подробности см. на странице https://go.microsoft.com/fwlink/?LinkID=317594.
     public class ApplicationUser : IdentityUser
     {
         [Display(Name = "Full Name"), Required]
@@ -15,9 +14,7 @@ namespace Auto_Service.Models
         
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
-            // Обратите внимание, что authenticationType должен совпадать с типом, определенным в CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Здесь добавьте утверждения пользователя
             return userIdentity;
         }
     }
